4b909427a591e363ba08584766bcdf301fe63764
gabrieltabosa <gabrieltabosa02@gmail.com>
2026-06-11 03:27:49 -0300

    adicionado dashboard semi-funcional, resolver classe css sendo escrita por cima

--- .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++ .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -1,5 +1,5 @@
 .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/gheys/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/gheys/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/gheys/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/gheys/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
+ /home/tabosa/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/tabosa/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/tabosa/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/tabosa/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/gheys/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/gheys/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
+ /home/tabosa/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/tabosa/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
